c523d71c0ae8acc0889703e35e57d39636eb7c31
jones-bass <jonesbass.tb@gmail.com>
2024-10-18 06:28:01 -0300

    Adjusting image field for upload

--- README.md
+++ README.md
@@ -1,36 +1,28 @@
-This is a [Next.js](https://nextjs.org) project bootstrapped with [`create-next-app`](https://nextjs.org/docs/app/api-reference/cli/create-next-app).
+## 🚀 reStore projeto com Next.js, ShadCN e Supabase!
 
-## Getting Started
+Iniciar o Projeto:
 
-First, run the development server:
+bash
+npm i
 
-```bash
-npm run dev
-# or
 yarn dev
-# or
-pnpm dev
-# or
-bun dev
-```
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+Nos últimos dias, estive desenvolvendo uma aplicação incrível utilizando *Next.js* como framework, *ShadCN* para um design de interface de usuário moderno e elegante, e *Supabase* para a base de dados e autenticação.
 
-You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
+## 🛠️ Principais tecnologias:
 
-This project uses [`next/font`](https://nextjs.org/docs/app/building-your-application/optimizing/fonts) to automatically optimize and load [Geist](https://vercel.com/font), a new font family for Vercel.
+- *Next.js*: Performance e renderização híbrida (SSG/SSR) tornam a aplicação altamente eficiente.
+- *ShadCN*: A biblioteca perfeita para criar uma UI rápida, bonita e acessível.
+- *Supabase*: Uma solução back-end poderosa, com banco de dados, autenticação e APIs em tempo real.
 
-## Learn More
+## 💡 Resultados
+O uso combinado dessas ferramentas resultou em uma aplicação escalável, rápida e com uma experiência de usuário fluida.
 
-To learn more about Next.js, take a look at the following resources:
+Se você está trabalhando com tecnologias modernas ou quer explorar novas possibilidades no desenvolvimento full-stack, recomendo muito dar uma olhada nessas ferramentas. ✨
 
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
+---
 
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/app/building-your-application/deploying) for more details.
+### Tecnologias utilizadas:
+- Next.js
+- ShadCN
+- Supabase
--- components/user-app/image-upload-placeholder.tsx
+++ components/user-app/image-upload-placeholder.tsx
@@ -1,12 +1,60 @@
+"use client"
+
 import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "../ui/dialog";
 import { Button } from "../ui/button";
-import { Podcast } from "lucide-react"; 
+import { Podcast } from "lucide-react";
+import { useCallback, useEffect, useState } from "react";
+
+import { useDropzone } from "react-dropzone";
+
+interface FilePreview {
+  file: Blob;
+  preview: string;
+}
 
 export default function ImageUploadPlaceholder() {
+  const [file, setFile] = useState<FilePreview | null>();
+  const [fileToProcess, setFileToProcess] = useState<{
+    path: string;
+  } | null>(null);
+
+  const [restoreFile, setRestoreFile] = useState<FilePreview | null>()
+
+  const onDrop = useCallback(async (acceptFiles: File[]) => {
+    try {
+      const file = acceptFiles[0];
+      setFile({
+        file,
+        preview: URL.createObjectURL(file),
+      });
+    } catch (error) {
+      console.log("onDrop", error)
+    }
+  }, []);
+
+  useEffect(() => {
+    return () => {
+      if (file) URL.revokeObjectURL(file.preview)
+      if (restoreFile) URL.revokeObjectURL(restoreFile.preview)
+    };
+  }, [])
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    onDrop,
+    maxFiles: 1,
+    accept: {
+      "image/png": [".png"],
+      "image/jpeg": [".jpg"],
+    }
+  })
+
+  const handleDialogOpenChange = async (e: boolean) => {
+    console.log(e)
+  }
   return (
     <div className="flex h-[200px] w-full shrink-0 items-center justify-center rounded-md border border-dashed">
       <div className="mx-auto flex max-w-[420px] flex-col items-center justify-center text-center">
-        <Podcast className="h-10 w-10 text-muted-foreground" /> 
+        <Podcast className="h-10 w-10 text-muted-foreground" />
 
         <h3 className="mt-4 text-lg font-semibold">Just add a Photo</h3>
         <p className="mb-4 mt-2 text-sm text-muted-foreground">
@@ -27,7 +75,47 @@ export default function ImageUploadPlaceholder() {
             </DialogHeader>
             <div className="grid gap-4 py-4">
               <div className="grid gap-2">
+                {!file && (
+                  <div {...getRootProps()}>
+                    <input {...getInputProps()} />
+                    {isDragActive ? (
+                      <p className="flex items-center justify-center bg-blue-100 opacity-70 border border-dashed border-blue-300 p-6 h-36 rounded-md">
+                        Drop your Photo here...</p>
+                    ) : (
+                      <p className="flex items-center justify-center bg-blue-100 opacity-70 border border-dashed border-blue-300 p-6 h-36 rounded-md">
+                        Drag or Click to choose image...</p>
+                    )}
+                  </div>
+                )}
+                <div className="flex flex-col items-center justify-evenly sm:flex-row gap-2">
+                  {
+                    file && (
+                      <div className="flex flex-row flex-wrap drop-shadow-md">
+                        <div className="flex w-48 h-48 relative">
+
+                          <img src={file.preview} className="w-48 h-48 object-contain rounded-md"
+                            onClick={() => URL.revokeObjectURL(file.preview)}
+                            alt="" />
+                        </div>
+                      </div>
+                    )
+                  }
+
+                  {
+                    restoreFile && (
+                      <div className="flex flex-row flex-wrap drop-shadow-md">
+                        <div className="flex w-72 h-7w-72 relative">
+
+                          <img src={restoreFile.preview} className="w-72 h-7w-72 object-contain rounded-md"
+                            onClick={() => URL.revokeObjectURL(restoreFile.preview)}
+                            alt="" />
+                        </div>
+                      </div>
+                    )
+                  }
+                </div>
               </div>
+
             </div>
             <DialogFooter>
               <Button>Enhance</Button>
@@ -35,6 +123,6 @@ export default function ImageUploadPlaceholder() {
           </DialogContent>
         </Dialog>
       </div>
-    </div>
+    </div >
   );
 }
